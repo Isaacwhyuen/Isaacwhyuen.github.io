848b29502dde7eb64c71c82d95d89fa93a02d8f1
Isaac Yuen <whyuenac@gmail.com>
2017-10-24 17:58:08 +0800

    animation added

--- main.js
+++ main.js
@@ -49,21 +49,6 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  // Scroll reveal calls
-  window.sr = ScrollReveal();
-  sr.reveal('.sr-icons', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 200);
-  sr.reveal('.sr-button', {
-    duration: 1000,
-    delay: 200
-  });
-  sr.reveal('.sr-contact', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 300);
+  $(document).snowfall({flakeCount : 400, maxSpeed : 10, shadow:true});
 
 })(jQuery); // End of use strict
