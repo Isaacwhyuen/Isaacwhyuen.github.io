b72af4953a5834bbe043c7e75f3918a13c47250f
Isaac Yuen <whyuenac@gmail.com>
2017-10-25 16:28:48 +0800

    updated

--- main.js
+++ main.js
@@ -49,9 +49,9 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  $(document).snowfall({ flakeCount: 200, maxSpeed: 10, shadow: true });
+  $(document).snowfall({ flakeCount: 100, maxSpeed: 10, shadow: true });
 
-  $('#profolio').snowfall({ flakeCount: 500, maxSpeed: 10, shadow: true });
+  $('#profolio').snowfall({ flakeCount: 100, maxSpeed: 10, shadow: true });
 
 
 })(jQuery); // End of use strict
